8ccd51676252dbf43b8ab76198f85bad8b5b65ac
Tejas Targhale <tejastarghale47@gmail.com>
2026-03-08 13:06:34 +0530

    Application has been updated, and properties also changed

--- app/src/main/java/com/example/demo/DemoApplication.java
+++ app/src/main/java/com/example/demo/DemoApplication.java
@@ -1,20 +1,51 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @SpringBootApplication
 @RestController
 public class DemoApplication {
 
+    @Value("${app.version:unknown}")
+    private String version;
+
+    @Value("${HOSTNAME:local}")
+    private String hostname;
+
     @GetMapping("/")
     public String home() {
-        return "Java App Running in Kubernetes!";
+        return "Java Application Running Successfully";
+    }
+
+    @GetMapping("/version")
+    public Map<String, String> version() {
+
+        Map<String, String> response = new HashMap<>();
+
+        response.put("application", "java-demo");
+        response.put("version", version);
+        response.put("pod", hostname);
+
+        return response;
+    }
+
+    @GetMapping("/health")
+    public String health() {
+        return "UP";
     }
 
     public static void main(String[] args) {
+
+        System.out.println("Starting Java Application Release...");
+
         SpringApplication.run(DemoApplication.class, args);
+
+        System.out.println("Application started successfully.");
     }
 }
